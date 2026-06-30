42a9eb325828c1dd3bca23a7e9b2b91ff3a7a63e
c0a2524429 <c0a2524429@edu.teu.ac.jp>
2026-06-30 15:07:27 +0900

    追加機能１実装完了

--- musou_kokaton.py
+++ musou_kokaton.py
@@ -242,11 +242,35 @@ class Score:
         screen.blit(self.image, self.rect)
 
 
+class Life(pg.sprite.Sprite):
+    """
+    残機数に関するクラス
+    num:残機数
+    """
+    def __init__(self,num:int):
+        super().__init__()
+        self.num = num
+        self.image = pg.Surface((40,40))
+        self.image.set_colorkey((0,0,0))
+        points = [(16*math.sin(t/100)**3 +20,
+                   -(13*math.cos(t/100)-5*math.cos(2*t/100)-2*math.cos(3*t/100)-math.cos(4*t/100)) +20) 
+                   for t in range(0, 628) ]
+        pg.draw.polygon(self.image, (255,0,0), points)
+        
+
+    def update(self, screen: pg.Surface):
+        for i in range(self.num):
+            x=WIDTH-50-50*i
+            y=  HEIGHT-50
+            screen.blit(self.image,(x,y))
+
+
 def main():
     pg.display.set_caption("真！こうかとん無双")
     screen = pg.display.set_mode((WIDTH, HEIGHT))
     bg_img = pg.image.load(f"fig/pg_bg.jpg")
     score = Score()
+    life = Life(3)
 
     bird = Bird(3, (900, 400))
     bombs = pg.sprite.Group()
@@ -283,11 +307,13 @@ def main():
             score.value += 1  # 1点アップ
 
         for bomb in pg.sprite.spritecollide(bird, bombs, True):  # こうかとんと衝突した爆弾リスト
-            bird.change_img(8, screen)  # こうかとん悲しみエフェクト
-            score.update(screen)
-            pg.display.update()
-            time.sleep(2)
-            return
+            life.num -= 1
+            if life.num<=0:
+                bird.change_img(8, screen)  # こうかとん悲しみエフェクト
+                score.update(screen)
+                pg.display.update()
+                time.sleep(2)
+                return
 
         bird.update(key_lst, screen)
         beams.update()
@@ -299,6 +325,7 @@ def main():
         exps.update()
         exps.draw(screen)
         score.update(screen)
+        life.update(screen)
         pg.display.update()
         tmr += 1
         clock.tick(50)
